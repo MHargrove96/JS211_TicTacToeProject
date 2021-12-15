83a6f1d7e9674860e7971a16adaa16ab06711f9d
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2021-12-14 22:01:39 -0600

    adding the score tracker

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -3,15 +3,25 @@ let board = [
   ['', '', ''],
   ['', '', ''],
   ['', '', '']]
-let p1Score = 0;
-let p2score = 0; 
 
-  window.onload = playerTurn = () => {
+window.onload = playerTurn = () => {
   document.getElementById('turnAnnouncement').innerHTML = 'Player ' + currentMarker + ' it is your turn!'
 }
 
-// is called when a square is clicked. "this" = element here
+let player1Score = 0; 
+let player2Score = 0;
+
+const playerScore = (score) => {
+ 
+  if(checkForWin() = true && currentMarker == 'X'){
+    score = document.getElementById('playerXScore').innerHTML = player1Score ++;
+  } else if(checkForWin() = true && currentMarker == 'O'){
+    score = document.getElementById('playerOScore').innerHTML = player2Score ++;
+  }
+}
+
 const handleClick = (element) => {
+  // is called when a square is clicked. "this" = element here
   console.log(`The element you clicked on has an id:  ${element.id}`)
   // check to see if the square clicked has anything in it, if not continue
   // this prevents an X being changed to an O
@@ -22,7 +32,7 @@ const handleClick = (element) => {
 }
 
 const addMarker = (id) => {
-  document.getElementById(id).innerHTML = currentMarker 
+  document.getElementById(id).innerHTML = currentMarker
   const [row, column] = id.split('-')
   console.log(`We'll place a ${currentMarker} on square: ${id}`)
   board[row][column] = currentMarker
@@ -32,10 +42,10 @@ const addMarker = (id) => {
 
 const checkForWin = () => {
   // calls each checkForWin possibility and if any are true gives a page alert,
-  if(horizontalWin() || verticalWin() || diagonalWin()) {
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
     window.alert(`Player ${currentMarker} won!`)
-    
-    
+    playerScore()
+
   } else {
     console.log('checkForWin was false')
     // if no win, change the marker from X to O, or O to X for the next player.
@@ -44,12 +54,12 @@ const checkForWin = () => {
 }
 
 const horizontalWin = () => {
-  if((board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
-  || (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
-  || (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
-  || (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
-  || (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
-  || (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')) {
+  if ((board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
+    || (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
+    || (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
+    || (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
+    || (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
+    || (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')) {
     return true
     console.log('H Win')
   }
@@ -57,12 +67,12 @@ const horizontalWin = () => {
 }
 
 const verticalWin = () => {
-  if((board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
-  || (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
-  || (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
-  || (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
-  || (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') 
-  || (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')) {
+  if ((board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
+    || (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
+    || (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
+    || (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
+    || (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
+    || (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')) {
     return true
     console.log('V Win')
   }
@@ -70,10 +80,10 @@ const verticalWin = () => {
 }
 
 const diagonalWin = () => {
-  if((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
-  || (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
-  || (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')
-  || (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')) {
+  if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
+    || (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+    || (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')
+    || (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')) {
     return true
     console.log('D Win')
   }
@@ -83,31 +93,21 @@ const diagonalWin = () => {
 const changeMarker = () => {
   // ternary operator: if it's an X make it an O, if O make it an X
   currentMarker = currentMarker === "X" ? "O" : "X"
- 
 }
 
-
-
 const resetBoard = () => {
-  // sanity check: this tells us the function is being called
-  // console.log("the board was cleared!")
-
-  // collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
   const squares = document.getElementsByTagName("TD")
-
   // loops over the HTML Collections and clears out the Xs and Os
   for (i = 0; i < squares.length; i++) {
     console.log(squares[i])
     squares[i].innerHTML = null
-  } 
+  }
   currentMarker = 'X'
   playerTurn()
   board = [
     ['', '', ''],
     ['', '', ''],
     ['', '', '']]
-  
-  // @TODO, Your code here: make sure to reset the array of arrays to empty for a new game
 }
 
 // **BONUSES**
